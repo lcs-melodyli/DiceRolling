81997a4713a73d06251de3db3845e39e04875c35
Melody Li <zli@lcs.on.ca>
2025-04-02 10:04:09 -0400

    Initial Commit

---
+++ DiceRolling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceRolling
+//
+//  Created by 利梓萌 on 2025-04-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRolling/DiceRollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollingApp.swift
+//  DiceRolling
+//
+//  Created by 利梓萌 on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
